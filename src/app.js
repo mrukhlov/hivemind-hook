7a1203d10f6aaba63b90a0a1f3a86fec0e553408
maxim <mrukhlov@speaktoit.com>
2016-09-11 08:16:58 +0300

    hook upd

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ restService.post('/webhook', (req, res) => {
 	let action = req.body.result.action;
 	console.log(action);
 	
-	let text_resp = 'Привет, я хайв бот, могу помочь тебе с учебой. Скажи что ты умеешь и по каким предметам тебе требуется помощь.'
+	let text_resp = 'aaa'
 	
 	/*if (action == 'bot_introduction'){
 		text_resp = 'Привет, я хайв бот, могу помочь тебе с учебой. Скажи что ты умеешь и по каким предметам тебе требуется помощь.'
